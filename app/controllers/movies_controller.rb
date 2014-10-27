33488f9215979e465db474eb661bf35ff364097c
Bri Dotson <bridotson@gmail.com>
2014-10-27 14:21:28 -0700

    move all the stuff I accidentally put in the movie model instead of the movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,26 @@
 class MoviesController < ApplicationController
-  
+  # index, show, edit, update, new, create, destroy
+  def index
+  end
+
+  def show
+    @movies = Movie.all
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+
+  end
+
+  def destroy
+  end
 end
