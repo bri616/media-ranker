51a645e50714b33703522fde6a82d59f508e73cb
Bri Dotson <bridotson@gmail.com>
2014-10-27 14:14:59 -0700

    add new method to movie controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,27 @@
 class Movie < ActiveRecord::Base
-  
+  # index, show, edit, update, new, create, destroy
+  def index
+  end
+
+  def show
+    @movies = Movie.all
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    
+  end
+
+  def destroy
+  end
+
 end
