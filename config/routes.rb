02c52bc3f91d9ef69ca830045ead8397ee603aa8
Bri Dotson <bridotson@gmail.com>
2014-10-29 11:23:29 -0700

    follow RESTful structure for movies routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   root "home#index"
+  # resources :movies
 
   # MOVIES
 
-  get   "/movies",            to: "movies#index",   as: :movies
-  post  "/movies",            to: "movies#create"
-  get   "/movies/new",        to: "movies#new",     as: :new_movie
-  get   "/movies/:id",        to: "movies#show",    as: :movie
-  get   "/movies/:id/edit",   to: "movies#edit",    as: :edit_movie
-  patch "/movies/:id",        to: "movies#update"
-  post  "/movies/:id",        to: "movies#destroy", as: :destroy_movie
-  post  "/movies/:id/upvote", to: "movies#upvote",  as: :upvote
+  get     "/movies",            to: "movies#index",   as: :movies
+  post    "/movies",            to: "movies#create"
+
+  get     "/movies/new",        to: "movies#new",     as: :new_movie
+
+  get     "/movies/:id",        to: "movies#show",    as: :movie
+  patch   "/movies/:id",        to: "movies#update"
+  put     "/movies/:id",        to: "movies#update"
+  delete  "/movies/:id",        to: "movies#destroy"
+
+  get     "/movies/:id/edit",   to: "movies#edit",    as: :edit_movie
+
+  post    "/movies/:id/upvote", to: "movies#upvote",  as: :upvote
 
 
   # The priority is based upon order of creation: first created -> highest priority.
